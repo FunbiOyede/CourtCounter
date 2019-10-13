086bbf89f62872dac3d4907ec6d07a848c8c9024
FunbiOyede <oyedetobi49@gmail.com>
2019-10-13 20:07:05 +0100

    implement points for the teams and start implmenting redux

--- src/container/Start/StartGame.jsx
+++ src/container/Start/StartGame.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Navigation from "../../components/Navigation/Navigation";
+import FirstTeam from "../Teams/FirstTeam";
+import SecondTeam from "../Teams/SecondTeam";
 
 class StartGame extends Component {
   state = {};
@@ -13,12 +15,13 @@ class StartGame extends Component {
   componentDidMount() {
     this.fetchTeams();
   }
+
   render() {
     return (
       <div>
         <Navigation />
-        <div>{this.state.TeamA}</div>
-        <div>{this.state.TeamB}</div>
+        <FirstTeam team={this.state.TeamA} />
+        <SecondTeam team={this.state.TeamB} />
       </div>
     );
   }
---
+++ src/container/Teams/SecondTeam.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class SecondTeam extends Component {
+  state = {
+    score: 0
+  };
+  ThreePointer = e => {
+    e.preventDefault();
+    this.setState({
+      score: this.state.score + 3
+    });
+  };
+
+  TwoPointer = e => {
+    e.preventDefault();
+    this.setState({
+      score: this.state.score + 2
+    });
+  };
+  FreeThrow = e => {
+    e.preventDefault();
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>{this.props.team}</h2>
+          <div>{this.state.score}</div>
+          <div>
+            <button onClick={this.ThreePointer}>+3 Points</button>
+            <button onClick={this.TwoPointer}>+2 Points</button>
+            <button onClick={this.FreeThrow}>Free Throw</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SecondTeam;
